0c565f4fd38d76cba2b2c01d23d9082ab210313c
ISHE04001 <77394101+ISHE04001@users.noreply.github.com>
2021-05-05 13:12:00 +0200

    Skapat repository

---
+++ Assets/Code/EnemyEyes.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyEyes : MonoBehaviour
+{
+
+    public float distance;
+    public LineRenderer LineOfSight;
+    public Gradient redColor;
+    public Gradient greenColor;
+    private void Start()
+    {
+        Physics2D.queriesStartInColliders = false;
+    }
+    void Update()
+    {
+
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, transform.right, distance);
+        if (hitInfo.collider != null)
+        {
+            Debug.DrawLine(transform.position, hitInfo.point, Color.red);
+            LineOfSight.SetPosition(1, hitInfo.point);
+            LineOfSight.colorGradient = redColor;
+            if (hitInfo.collider.CompareTag("Player"))
+            {
+                Destroy(hitInfo.collider.gameObject);
+            }
+            else
+            {
+                Debug.DrawLine(transform.position, transform.position + transform.right * distance, Color.green);
+                LineOfSight.SetPosition(1, transform.position + transform.right * distance);
+                LineOfSight.colorGradient = greenColor;
+            }
+            LineOfSight.SetPosition(0, transform.position);
+        }
+    }
+}
+
---
+++ Assets/Code/NPC.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    public float speed;
+    public float distance;
+    private bool movingRight = true;
+
+    public Transform groundDetection;
+
+    bool isGrounded = false;
+    public Transform isGroundedChecker;
+    public float checkGroundRadius;
+    public LayerMask groundLayer;
+
+    
+
+    void Start()
+    {
+        GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        CheckIfGrounded();
+
+        void CheckIfGrounded()
+        {
+            Collider2D colliders = Physics2D.OverlapCircle(isGroundedChecker.position, checkGroundRadius, groundLayer);
+            if (colliders != null)
+            {
+                isGrounded = true;
+
+            }
+            else
+            {
+                if (isGrounded)
+                {
+
+                }
+                isGrounded = false;
+            }
+        }
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        int layer_mask = LayerMask.GetMask("Ground");
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance, layer_mask);
+        if (groundInfo.collider == false)
+        {
+            if (movingRight == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+
+
+
+
+            }
+           
+            }
+        }
+    }
+
+
+
